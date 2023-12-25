eecf5df67bcde0ce574380c526854058a95eb926
Harjot Singh <HarjjotSinghh@gmail.com>
2023-12-26 00:11:16 +0530

    Added Google AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { cn } from '@/lib/utils';
 import Footer from '@/components/component/footer';
 import Header from '@/components/component/header';
+import Script from 'next/script';
 
 const outfit = Outfit({variable: "--font-outfit", subsets:["latin"]});
 
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
   title: 'Brainchant',
   description: 'Brainchant\'s official website, providing useful resources and study material to all IPU students.',
   keywords: "notes, instagram notes ideas, instagram notes, ipu notes, btech notes ipu, ipu btech notes, btech notes, brainchant, brain chant, brainchant ipu notes",
+  
 }
 
 export default function RootLayout({
@@ -20,6 +22,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-9527505327551646"
+        async
+        strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          crossOrigin='anonymous'
+      />
+      </head>
       <body className={cn(
           "min-h-screen antialiased bg-background text-foreground",
           outfit.className
